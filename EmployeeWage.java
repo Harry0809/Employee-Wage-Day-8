5d1783f3381fea6232a2d785fd10f299a64151d4
Harish mahur <hmahur20@gmail.com>
2022-07-22 07:04:38 +0530

    modified:Welcome to employee wages computation day-8

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,14 +4,33 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" Welcome to employee wage computation day-8");
-		int attandance = 1;
-		double randomCheck = Math.floor(Math.random() * 10) % 2;
-		if (attandance == randomCheck) {
-			System.out.println("Employee is present");
-		} else {
-			System.out.println("Employee is absent");
-		}
-	}
+		System.out.println(" Welcome to employee wage computation");
 
+		int wagePerHour = 20;
+		int numberOfHourPerFullDay = 8;
+		int numberOfHourPerPartDay = 4;
+		int wageOfTheDay = 0;
+
+		int employeeAttendance = (int) (Math.floor(Math.random() * 10)) % 2;
+		switch (employeeAttendance) {
+
+		case 1:
+			System.out.println("Emoloyee is working full day");
+			wageOfTheDay = numberOfHourPerFullDay * wagePerHour;
+			System.out.println("full day wage of the employee is" + " " + wageOfTheDay);
+			break;
+
+		case 2:
+			System.out.println("Employee is working part day");
+			wageOfTheDay = numberOfHourPerPartDay * wagePerHour;
+			System.out.println("Part time  wage of employee is" + " " + wageOfTheDay);
+			break;
+
+		default:
+			System.out.println("employee is not working today");
+			break;
+
+		}
+
+	}
 }
